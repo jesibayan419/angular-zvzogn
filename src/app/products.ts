d42c57fafdd21abc864a5b2a5cddcc285d3c3364
jesibayan419 <97929978+jesibayan419@users.noreply.github.com>
2023-03-16 14:35:41 +0800

    4 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -17,34 +17,39 @@ export const products = [
     id: 2,
     name: 'Samsung Phone',
     price: 699,
-    description: 'Like iPhone, but not iPhone.'
+    description: 'Like iPhone, but not iPhone.',
+    image: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1647622740-samsung-galaxy-a53-1647622731.jpg'
   },
   {
     id: 3,
     name: 'Xiaomi Phone',
     price: 299,
-    description: 'You can get a good phone even if you are broke.'
+    description: 'You can get a good phone even if you are broke.',
+    image: 'https://i.expansys.net/img/b/393175/xiaomi-13-5g-dual-sim.jpg'
   },
 
   {
     id: 4,
     name: 'Oppo Phone',
     price: 299,
-    description: 'Meh, average phone for the price.'
+    description: 'Meh, average phone for the price.',
+    image: 'https://i.expansys.net/img/p/372317/oppo-reno-7-5g-dual-sim.jpg'
   },
 
   {
     id: 5,
     name: 'Vivo Phone',
     price: 299,
-    description: 'Same as Oppo.'
+    description: 'Same as Oppo.',
+    image: 'https://www.vivoglobal.ph/wp-content/uploads/2022/12/Y02-%E9%A2%9C%E8%89%B2%E5%9B%BE.jpg'
   },
 
   {
     id: 6,
     name: 'Realme Phone',
     price: 299,
-    description: 'Had a spark when first started, but now iz just garbage.'
+    description: 'Had a spark when first started, but now iz just garbage.',
+    image: 'https://i.expansys.net/img/p/374939/realme-gt-neo-2-5g-dual-sim.jpg'
   },
 ];
 
